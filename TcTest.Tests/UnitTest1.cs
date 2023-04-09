1a18aa402334a8d4808908dba7326b772fd45957
StephanKumpitsch <stephan.kumpitsch@innerspace.eu>
2023-04-09 19:37:15 +0200

    [ADD] tests

---
+++ TcTest.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TcTest.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.IsTrue(true);
+    }
+}
