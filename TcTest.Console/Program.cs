462ba3bb9c40ad7727c9892bbff054250a6d30f7
StephanKumpitsch <stephan.kumpitsch@innerspace.eu>
2023-05-16 19:35:33 +0200

    FEAT-3 added message based on FEAT-2

--- TcTest.Console/Program.cs
+++ TcTest.Console/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Hello, I'm a feature!");
+Console.WriteLine("Tralala");
+Console.WriteLine("Staging");
+
 Console.WriteLine("Adding FEAT-3 based on FEAT-2");
-Console.WriteLine("-----------------------------");
